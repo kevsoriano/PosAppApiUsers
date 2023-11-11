4602dc277803246066a00e5609897aaf76c89b1c
kevingilsoriano <kevingilsoriano@localhost>
2023-11-11 22:04:52 +0800

    update ip address

--- src/main/java/com/jkngil/pos/users/data/UserEntity.java
+++ src/main/java/com/jkngil/pos/users/data/UserEntity.java
@@ -2,7 +2,6 @@ package com.jkngil.pos.users.data;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 import com.jkngil.pos.users.shared.UserDto;
